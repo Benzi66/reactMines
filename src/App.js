8e107652220c6980b94d424119763e433a93379b
Benzi66 <Avicbensimon@gmail.com>
2025-05-25 14:48:45 +0300

    idkfr

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+var turns = 0;
+function Square({value1}) {
+  const [value , setValue] = useState(value1);
+  function handleClick() {
+    value === "X" ? setValue(null) :
+      setValue("X");
+      turns++;
+      console.log(turns);
+  }
+  return <button className="square"
+    onClick={handleClick}>
+    {value}
+  </button>;
+}
+
+export default function Board() {
+  const v = "n";
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="board-row">
+        <Square value={v} />
+        <Square />
+        <Square />
+      </div>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+    </>
   );
 }
 
-export default App;
