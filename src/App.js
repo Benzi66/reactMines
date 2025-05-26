034fcae047b9a74b0ec8d3ea8f264340b75730ec
Benzi66 <Avicbensimon@gmail.com>
2025-05-26 14:37:00 +0300

    added cash system

--- src/App.js
+++ src/App.js
@@ -9,11 +9,21 @@ export default function MineField() {
   const [mines, setMines] = useState(Array(25).fill("V"));
   const [minesAmt, setAmt] = useState(1);
   const [mult, setMult] = useState(1);
+  const [currCash,setCurrCash] = useState(0);
+  const [TotalCash, setTotalCash] = useState(300);
+
+  function CashOut() {
+    setGameStarted(false);
+    setCurrCash(currCash * mult);
+    setTotalCash(TotalCash + currCash);
+    setCurrCash(0);
+  }
   function StartButton() {
     setSquares(Array(25).fill(null));
     PlaceMines({Amount: minesAmt});
     setGameStarted(true);
     setMult(1);
+    setTotalCash(TotalCash-currCash);
   }
   function PlaceMines({ Amount }) {
     const nextMines = Array(25).fill("V");
@@ -45,6 +55,8 @@ export default function MineField() {
       nextSquares[i] = mines[i];
       if (nextSquares[i] === "X") {
         setGameStarted(false);
+        setCurrCash(0);
+        setMult(1);
         for (let t = 0; t < 25; t++) {
           nextSquares[t] = mines[t]
         }
@@ -60,10 +72,14 @@ export default function MineField() {
 
   return (
     <>
-
+      
       <button onClick={StartButton} disabled = {gameStarted}>Start</button>
+      <button onClick={CashOut} disabled = {!gameStarted}>Cash Out</button>
       <h4>Multiplier: {mult}</h4>
+      <h4>TotalCash: {TotalCash}</h4>
+      <h4>Cash Out Money: {parseFloat((mult * currCash).toFixed(2))}</h4>
       <NumberInput label={"Amount of mines"} value = {minesAmt} onChange={setAmt} disabled = {gameStarted} />
+      <NumberInput label={"Cash for game"} value = {currCash} onChange={setCurrCash} disabled = {gameStarted} />
       <GameBoard squares={squares} onClick={handleClick} boardSize={5} gameStarted={!gameStarted}/>
     </>
   );
